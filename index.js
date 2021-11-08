d834784202b8db16f375b5d923b55bdba8d39e6e
Acevedo Kevin <87038684+Kesocial@users.noreply.github.com>
2021-11-08 15:31:59 -0300

    a

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get("/juego", (req, res) => {
     <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Tp 16</title>
+        <title>Digitos Directos</title>
         <link rel="stylesheet" href="css/main.css" />
     </head>
     
